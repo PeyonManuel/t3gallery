5d949148d69f7f70c34a902441f5d2cd2a5efb97
unknown <peyonmanuel@gmail.com>
2024-11-19 11:10:47 +0100

    scaffold UI

--- README.md
+++ README.md
@@ -30,6 +30,14 @@ Follow our deployment guides for [Vercel](https://create.t3.gg/en/deployment/ver
 
 # T3 Gallery
 
-- [ ] Make it deploy (vercel)
+- [x] Make it deploy (vercel)
 - [ ] Scaffold basic ui with mock data
-- [ ] Actually set up database (vercel)
+- [ ] Actually set up database (vercel postgress)
+- [ ] Attach database to UI
+- [ ] Add authentication (w/ clerk)
+- [ ] Add image upload
+- [ ] Error management (w/ Sentry)
+- [ ] Routing/image page (parallel route)
+- [ ] Delete button (w/ Server Actions)
+- [ ] Analytics (posthog)
+- [ ] Ratelimiting (upstash)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,31 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/hYCuJzM7MA50NHSqrYkAThCcRYZ4vtuzNSEd8K5fpxUJlIM0",
+  "https://utfs.io/f/hYCuJzM7MA50Vo2pEVnCkvqGjDZR30Ti5WzybVY8r2nlSM6p",
+  "https://utfs.io/f/hYCuJzM7MA50wNcndHmBMtJSRTkNjbqFg19lKdDezwX7uG35",
+  "https://utfs.io/f/hYCuJzM7MA50jVqxizEPzB8yeTLpMfiObFQDWkag6Rv3GKZt",
+  "https://utfs.io/f/hYCuJzM7MA50yJLmGBdNoCtHek5dpAVarYzmSXwOIhLnZ8yi",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+  title: "title",
+  description: "description",
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-          Hello gallery in progress
-        </h1>
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt={image.title} />
+          </div>
+        ))}
       </div>
+      Hello gallery in progress
     </main>
   );
 }
