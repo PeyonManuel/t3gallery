262c5a3071f65b460871ee82197d076dd24daadc
unknown <peyonmanuel@gmail.com>
2024-11-21 11:48:09 +0100

    finish modal

--- src/app/_components/full-image-page.tsx
+++ src/app/_components/full-image-page.tsx
@@ -1,3 +1,4 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { getMyImage } from "~/server/queries";
 
 export default async function FullImagePageView({
@@ -8,17 +9,25 @@ export default async function FullImagePageView({
   const { id: imgId } = await params;
 
   const image = await getMyImage(imgId);
+  const clerk = await clerkClient();
+  const uploaderInfo = await clerk.users.getUser(image.userId);
   return (
     <div className="flex h-full w-full min-w-0">
-      <div className="flex max-w-screen-lg flex-shrink items-center justify-center">
+      <div className="flex flex-shrink items-center justify-center">
         <img
           className="flex-shrink object-contain"
           src={image.url!}
           alt={image.name!}
         />
       </div>
-      <div className="flex w-48 flex-shrink-0 flex-col">
-        <h1 className="text-2xl font-bold">{image.name}</h1>
+      <div className="flex w-48 flex-shrink-0 flex-col border-l">
+        <div className="border-b p-2 text-center text-lg">{image.name}</div>
+        <div className="flex flex-col p-2">
+          <span>Uploaded By:</span>
+          <span>{uploaderInfo.fullName}</span>
+          <span>Created On</span>
+          <span>{new Date(image.createdAt).toLocaleDateString()}</span>
+        </div>
       </div>
     </div>
   );
